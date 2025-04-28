9d3783ca15c8f69ca361bcc314d7279a526435c9
Matteo Brambilla <bramba2000@gmail.com>
2025-04-28 18:19:01 +0200

    refactor delete command

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -3,18 +3,14 @@ package main
 import (
 	"dspn-regogenerator/cmd/cli/commands"
 	"dspn-regogenerator/config"
-	"dspn-regogenerator/internal/bundle"
 	"fmt"
 	"os"
-	"path/filepath"
-
-	opabundle "github.com/open-policy-agent/opa/v1/bundle"
 
 	"github.com/spf13/cobra"
 )
 
 func main() {
-	var serviceName, bucketName, minioServer, minioSecretKey, minioAccessKey string
+	var bucketName, minioServer, minioSecretKey, minioAccessKey string
 
 	var rootCmd = &cobra.Command{Use: "dspn-regogenerator"}
 
@@ -47,55 +43,9 @@ func main() {
 	configCmd.MarkFlagRequired("minio_access_key")
 	configCmd.MarkFlagRequired("minio_secret_key")
 
-	var localPath = ""
+	rootCmd.AddCommand(configCmd)
 
-	var DeleteServicePolicyCmd = &cobra.Command{
-		Use:   "delete",
-		Short: "Delete policies related to a service",
-		Run: func(cmd *cobra.Command, args []string) {
-			var b = new(opabundle.Bundle)
-			var err error
-			if localPath != "" {
-				b, err = bundle.LoadBundleFromFile(localPath)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "Error loading bundle from local path: %v\n", err)
-					os.Exit(1)
-				}
-			} else {
-				b, err = bundle.LoadBundleFromMinio(config.Config.BundleFileName)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "Error loading bundle from Minio: %v\n", err)
-					os.Exit(1)
-				}
-			}
-			newBundle, err := bundle.RemoveService(b, serviceName)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error removing service: %v\n", err)
-				os.Exit(1)
-			}
-			files := bundle.ListBundleFiles(newBundle)
-			services := map[string]struct{}{}
-			fmt.Println("Remaining services")
-			for _, f := range files {
-				services[filepath.Dir(f)] = struct{}{}
-			}
-			for k := range services {
-				fmt.Println(k)
-			}
-			err = bundle.WriteBundleToMinio(newBundle, config.Config.BundleFileName)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error writing bundle to Minio: %v\n", err)
-				os.Exit(1)
-			}
-		},
-	}
-	DeleteServicePolicyCmd.Flags().StringVar(&serviceName, "service_name", "", "Name of the service (required)")
-	DeleteServicePolicyCmd.MarkFlagRequired("service_name")
-	DeleteServicePolicyCmd.Flags().StringVar(&localPath, "local-path", "", "")
-
-	rootCmd.AddCommand(configCmd, DeleteServicePolicyCmd)
-
-	rootCmd.AddCommand(commands.AddCmd, commands.ListCmd)
+	rootCmd.AddCommand(commands.AddCmd, commands.ListCmd, commands.DeleteCmd)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
