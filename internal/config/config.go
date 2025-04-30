65327515acf0e90a1da04b2d6dcb5ac26bcb4d47
Matteo Brambilla <bramba2000@gmail.com>
2025-04-30 15:34:26 +0200

    fix wrong env name

--- internal/config/config.go
+++ internal/config/config.go
@@ -16,7 +16,7 @@ func GetEnvOrDefault(key, defaultValue string) string {
 
 var (
 	// Address of the MinIO server, without the protocol (http:// or https://).
-	// The default value is "localhost:9000", load from environment variable MINIO_ENDPOINT.
+	// The default value is "localhost:9000", load from environment variable MINIO_SERVER.
 	MinioEndpoint string
 
 	// The access key for MinIO.
@@ -28,7 +28,7 @@ var (
 	MinioSecretKey string
 
 	// The bucket name where to store the bundles.
-	// The default value is "opa-policy-bundles", load from environment variable MINIO_BUCKET.
+	// The default value is "opa-policy-bundles", load from environment variable BUCKET_NAME.
 	MinioBucket string
 
 	// The bundle name prefix, used to create the bundle name adding a -version tag suffix.
@@ -48,10 +48,10 @@ var (
 
 // ReloadConfig initializes or reloads the global variables based on the current environment variables. There is no need to call this function manually, as it is automatically called when the package is loaded.
 func ReloadConfig() {
-	MinioEndpoint = GetEnvOrDefault("MINIO_ENDPOINT", "localhost:9000")
+	MinioEndpoint = GetEnvOrDefault("MINIO_SERVER", "localhost:9000")
 	MinioAccessKey = GetEnvOrDefault("MINIO_ACCESS_KEY", "admin")
 	MinioSecretKey = GetEnvOrDefault("MINIO_SECRET_KEY", "adminadmin")
-	MinioBucket = GetEnvOrDefault("MINIO_BUCKET", "opa-policy-bundles")
+	MinioBucket = GetEnvOrDefault("BUCKET_NAME", "opa-policy-bundles")
 	MinioBundlePrefix = GetEnvOrDefault("MINIO_BUNDLE_PREFIX", "teadal-policy-bundle")
 	LatestBundleName = MinioBundlePrefix + "-LATEST.tar.gz"
 	TagBundleName = func(tag string) string {
