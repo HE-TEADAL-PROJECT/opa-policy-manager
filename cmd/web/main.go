1e536785e18205ced090eb8a5aafb6b14ed94624
Matteo Brambilla <bramba2000@gmail.com>
2025-05-07 10:45:48 +0200

    add test when starting the web server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"context"
 	"dspn-regogenerator/cmd/web/handlers"
+	"dspn-regogenerator/internal/usecases"
 	"errors"
 	"log/slog"
 	"net/http"
 )
 
 func main() {
+	slog.Info("Starting OPA policy manager")
+
+	// Perform initial tests
+	if err := usecases.InitialTest(context.TODO()); err != nil {
+		slog.Error("Initial test failed", "error", err)
+		return
+	}
+	slog.Info("Initial test passed")
+
+	// Configure and start the HTTP server
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /api/policies", handlers.ListServicePolicies)
 	mux.HandleFunc("PUT /api/policies", handlers.AddServicePolicies)
