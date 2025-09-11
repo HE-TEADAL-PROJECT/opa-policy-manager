27e6ff65cbba3a1ef15b5e4b91249d65e1358dd2
Matteo Brambilla <bramba2000@gmail.com>
2025-09-11 17:44:02 +0200

    Add bundle creation to test and update Docker Compose for bundle usage

--- test/e2e/bundle/bundle_test.go
+++ test/e2e/bundle/bundle_test.go
@@ -2,12 +2,33 @@ package bundle
 
 import (
 	"context"
+	"dspn-regogenerator/internal/bundle"
+	"dspn-regogenerator/internal/policy/parser"
 	"testing"
 
 	"github.com/testcontainers/testcontainers-go/modules/compose"
 )
 
-func TestBundleAuthorization(t *testing.T) {
+const bundlePath = "./bundle.tar.gz"
+
+func createTestBundle(t testing.TB) {
+	t.Helper()
+	service := bundle.NewService("httpbin", &parser.ServiceSpec{
+		Policies:         parser.StructuredPolicies{},
+		IdentityProvider: "http://127.0.0.1:5555",
+	})
+	b, err := bundle.New(*service)
+	if err != nil {
+		t.Fatalf("failed to create bundle: %v", err)
+	}
+	repo := bundle.FSRepository{}
+	if err := repo.Save(bundlePath, *b); err != nil {
+		t.Fatalf("failed to save bundle: %v", err)
+	}
+}
+
+func createStack(t testing.TB) *compose.DockerCompose {
+	t.Helper()
 	stack, err := compose.NewDockerComposeWith(
 		compose.StackIdentifier("test-e2e-bundle"),
 		compose.WithStackFiles("./docker-compose.yaml"),
@@ -24,4 +45,10 @@ func TestBundleAuthorization(t *testing.T) {
 			t.Fatalf("Failed to stop Docker Compose stack: %v", err)
 		}
 	})
+	return stack
+}
+
+func TestBundleAuthorization(t *testing.T) {
+	createTestBundle(t)
+	createStack(t)
 }
